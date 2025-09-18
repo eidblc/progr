47ac5a456fcd75caa245c74c0c7badec25566350
eidblc <eidvydas.balc@gmail.com>
2025-09-18 09:47:59 +0300

    naujas failas

---
+++ studentas.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+    string vardas = "Eidvydas";
+    string pavarde = "Balcius";
+    string amzius = "18";
+    string grupe = "PI25";
+    string kursas = "Pirmas";
+    string stud_prog = "Programu sistemos";
+
+    cout << vardas << " " << pavarde << endl;
+    cout << "Amzius: " << amzius << endl;
+    cout << "Grupe: " << grupe << endl;
+    cout << "Kursas: " << kursas << endl;
+    cout << "Studiju programa: " << stud_prog << endl;
+    return 0;
+}
